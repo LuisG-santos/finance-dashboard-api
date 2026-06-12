6a0368983ce72440817b78ab043fbac26449d581
Luis Gustavo <luisgustavo.nunes@icloud.com>
2026-06-12 11:54:24 -0300

    fix: fix import method

--- src/use-cases/createUser.js
+++ src/use-cases/createUser.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 import { v4 as uuidv4 } from 'uuid';
-import { PostgresCreateUserRepository } from '../repository/postgres/createUser';
+import { PostgresCreateUserRepository } from '../repository/postgres/createUser.js';
 
 export class CreateUserUseCase {
   async execute(createUserParams) {
