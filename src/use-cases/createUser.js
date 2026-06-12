14e467d14a847ff7eb5e9a96cbecfdc3b8ece99e
Luis Gustavo <luisgustavo.nunes@icloud.com>
2026-06-11 21:49:55 -0300

    feat: add Use-Case for create user

---
+++ src/use-cases/createUser.js
@@ -0,0 +1,20 @@
+import bcrypt from 'bcrypt';
+import { v4 as uuidv4 } from 'uuid';
+import { PostgresCreateUserRepository } from '../repository/postgres/createUser';
+
+export class CreateUserUseCase {
+  async execute(createUserParams) {
+    const userId = uuidv4();
+    const hashedPassword = await bcrypt.hash(createUserParams.password, 10);
+
+    const user = {
+      ...createUserParams,
+      id: userId,
+      password: hashedPassword,
+    };
+
+    const postgresCreateUserRepository = new PostgresCreateUserRepository();
+    const createdUser = await postgresCreateUserRepository.execute(user);
+    return createdUser;
+  }
+}
