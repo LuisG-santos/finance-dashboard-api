1896f755385f15c4020e083990944de5f04f7e63
Luis Gustavo <luisgustavo.nunes@icloud.com>
2026-06-14 20:28:59 -0300

    feat: implement UpdateUser use case for updating user details

---
+++ src/use-cases/updateUser.js
@@ -0,0 +1,30 @@
+import { EmailAlreadyInUseError } from '../errors/users.js';
+import { GetUserByEmail } from '../repository/postgres/getUserByEmail.js';
+import bcrypt from 'bcrypt';
+import { PostgresUpdateUserRepository } from '../repository/postgres/updateUser.js';
+export class UpdateUseruseCase {
+  async execute(userId, upadateUserParams) {
+    if (upadateUserParams.email) {
+      const getUserByEmail = new GetUserByEmail();
+
+      const userWithProvidedEmail = await getUserByEmail.execute(upadateUserParams.email);
+
+      if (userWithProvidedEmail) {
+        throw new EmailAlreadyInUseError(upadateUserParams.email);
+      }
+    }
+    const user = {
+      ...upadateUserParams,
+    };
+
+    if (upadateUserParams.password) {
+      const hashedPassword = await bcrypt.hash(upadateUserParams.password, 10);
+      user.password = hashedPassword;
+    }
+
+    const postgresUpdateUserRepository = new PostgresUpdateUserRepository();
+    const updatedUser = await postgresUpdateUserRepository.execute(userId, user);
+
+    return updatedUser;
+  }
+}
