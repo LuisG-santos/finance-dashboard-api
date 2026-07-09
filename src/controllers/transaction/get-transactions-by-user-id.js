91f86bebc372b21ee9d85ee3543828c621efb743
Luis Gustavo <luisgustavo.nunes@icloud.com>
2026-07-08 22:00:09 -0300

    feat: implement GetTransactionsByUserIdController to handle user transaction requests

---
+++ src/controllers/transaction/get-transactions-by-user-id.js
@@ -0,0 +1,41 @@
+import { UserNotFoundError } from '../../errors/users.js';
+import {
+  userNotFoundResponse,
+  serverError,
+  requiredFieldIsMissingResponse,
+  checkIfIdIsValid,
+  InvalidIdResponse,
+  ok,
+} from '../helpers';
+
+export class GetTransactionsByUserIdController {
+  constructor(getTransactionByUserIdUseCase) {
+    this.getTransactionsByUserIdUseCase = getTransactionByUserIdUseCase;
+  }
+
+  async execute(httpRequest) {
+    try {
+      const userId = httpRequest.query.userId;
+
+      if (!userId) {
+        return requiredFieldIsMissingResponse('userId');
+      }
+
+      if (!checkIfIdIsValid(userId)) {
+        return InvalidIdResponse();
+      }
+
+      const transactions = await this.getTransactionsByUserIdUseCase.execute({ userId });
+
+      return ok(transactions);
+    } catch (error) {
+      console.log(error);
+
+      if (error instanceof UserNotFoundError) {
+        return userNotFoundResponse();
+      }
+
+      return serverError();
+    }
+  }
+}
