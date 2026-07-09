c5ec714938d9a1892c3f275af6dcff3377dede63
Luis Gustavo <luisgustavo.nunes@icloud.com>
2026-07-08 21:48:08 -0300

    feat: add GetTransactionsByUserId use case and update exports

--- src/use-cases/index.js
+++ src/use-cases/index.js
@@ -2,3 +2,5 @@ export * from './user/createUser.js';
 export * from './user/getUserById.js';
 export * from './user/updateUser.js';
 export * from './user/deleteUser.js';
+export * from './transaction/createTransaction.js';
+export * from './transaction/get-transactions-by-user-id.js';
